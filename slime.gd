3b4ad7000b0d6ffb2928b1ca2613a22240a374cd
Maximistr <maximistr100@gmail.com>
2025-04-06 11:46:34 +0200

    spawn

--- slime.gd
+++ slime.gd
@@ -5,21 +5,27 @@ const SPEED = 300.0
 const JUMP_VELOCITY = -500.0
 var hit_val = 0
 var upg = []
+var can_do = false
+
+func _ready() -> void:
+	$Sprite.play("spawn")
+	await get_tree().create_timer(1).timeout
+	can_do = true
 
 func _physics_process(delta: float) -> void:
-	if not is_on_floor():
+	if not is_on_floor() and can_do == true:
 		velocity += get_gravity() * delta
 		$Sprite.play("jump")
-	if is_on_floor():
+	if is_on_floor() and can_do == true:
 		$Sprite.play("walk")
 
 	var direction := Input.get_axis("left", "right")
-	if direction:
+	if direction and can_do == true:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		
-	if Input.is_action_just_pressed("atk"):
+	if Input.is_action_just_pressed("atk") and can_do == true:
 		$attack.play("default")
 		
 	if $early.has_overlapping_areas() == true:
@@ -32,7 +38,7 @@ func _physics_process(delta: float) -> void:
 		hit_val = 0
 		
 		
-	if Input.is_action_just_pressed("atk") and hit_val > 0 and not is_on_floor():
+	if Input.is_action_just_pressed("atk") and hit_val > 0 and not is_on_floor() and can_do == true:
 		velocity.y = 0
 		kill()
 		if hit_val == 2:
@@ -44,13 +50,15 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("1"):
 		upg[5] = 1
 
-	if Input.is_action_just_pressed("atk") and is_on_floor():
+	if Input.is_action_just_pressed("atk") and is_on_floor() and can_do == true:
 		velocity.y = JUMP_VELOCITY + 50
 	
 	move_and_slide()
 
 func death():
-	pass
+	can_do = false
+	$Sprite.play("death")
+	get_tree().reload_current_scene()
 
 func _on_jelly_area_entered(area: Area2D) -> void:
 	var e = area#.get_parent()
