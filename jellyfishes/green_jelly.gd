98dfdcd6f91e818fa304c0cb3d1837baeddca603
Lukáš <lukassesk62@gmail.com>
2025-04-06 11:43:48 +0200

    freaky jellyfishes
    commit suicide

--- jellyfishes/green_jelly.gd
+++ jellyfishes/green_jelly.gd
@@ -1,9 +1,9 @@
 extends CharacterBody2D
 
 
-const SPEED = 100
+const SPEED = 69
 var direction = true
-
+var e = true
 
  
 
@@ -12,6 +12,10 @@ func _physics_process(delta: float) -> void:
 		velocity.x = SPEED
 	else:
 		velocity.x = SPEED*-1
-
+	if is_on_wall() and e == true:
+		direction = !direction
+		e = false
+		await get_tree().create_timer(0.5).timeout
+		e = true
 
 	move_and_slide()
