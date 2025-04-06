61e515bc53f0eba5768b6ff04a3d6d7a872d669b
Lukáš <lukassesk62@gmail.com>
2025-04-06 14:45:49 +0200

    jellyfish
    enemy jellyfishes all kinds

--- jellyfishes/title_card_jellyfish.gd
+++ jellyfishes/title_card_jellyfish.gd
@@ -1,34 +1,23 @@
 extends CharacterBody2D
 var allow_fade_out := true
-
-
-
-func _ready() -> void:
-	$Sprite/Timer.start()
-	_fade_loop()
+var g = true
 
 
 func _process(delta: float) -> void:
-	#$Sprite.modulate.a = $Sprite/Timer.time_left
-	if allow_fade_out:
-		$Sprite.modulate.a = $Sprite/Timer.time_left / $Sprite/Timer.wait_time
-
-func fade_in():
-	$Sprite/Timer.start()
-	await $Sprite/Timer.timeout
-	while $Sprite/Timer.time_left > 0:
-		$Sprite.modulate.a = 1.0 - ($Sprite/Timer.time_left / $Sprite/Timer.wait_time)
-		await get_tree().process_frame
-	$Sprite.modulate.a = 1.0
-#func _on_Timer_timeout():
-	#$Sprite/Timer.stop()
-	#allow_fade_out = false
-	#await fade_in()
-func _fade_loop():
-	while true:
-		allow_fade_out = true
-		$Sprite/Timer.start()
-		await $Sprite/Timer.timeout
-		allow_fade_out = false
-		await fade_in()
-		await get_tree().create_timer(1.0).timeout  # wait 1 second before repeating
+	#print($Sprite.modulate.a)
+	#$Spritmodulate.a = $Sprite/Timer.time_left
+	if $Sprite.modulate.a > 0:
+		$Hit_area/hit.disabled = false
+		$sus/body.disabled = false
+	if $Sprite.modulate.a >= 1:
+		g = true
+	elif $Sprite.modulate.a <= 0:
+		$Hit_area/hit.disabled = true
+		$sus/body.disabled = true
+		await get_tree().create_timer(1).timeout
+		g = false
+	if g == true:
+		$Sprite.modulate.a -= 0.5 * delta
+	if g == false:
+		$Sprite.modulate.a += 0.5 * delta
+	
