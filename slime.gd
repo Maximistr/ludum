603c39be51e867ebba160076e5afdf65b9e4172d
Maximistr <maximistr100@gmail.com>
2025-04-06 17:36:50 +0200

    atk cooldown

--- slime.gd
+++ slime.gd
@@ -5,18 +5,24 @@ const SPEED = 300.0
 const JUMP_VELOCITY = -500.0
 var hit_val = 0
 var can_do = false
+var can_atk = true
 
 func _ready() -> void:
 	$Sprite.play("spawn")
 	await get_tree().create_timer(1).timeout
 	can_do = true
+	can_atk = true
 
 func _physics_process(delta: float) -> void:
+	print(can_atk)
 	if not is_on_floor() and can_do == true:
 		velocity += get_gravity() * delta
 		$Sprite.play("jump")
 	if is_on_floor() and can_do == true:
 		$Sprite.play("walk")
+		
+	if $"atk cooldown".time_left > 0:
+		can_atk = false
 
 	var direction := Input.get_axis("left", "right")
 	if direction and can_do == true:
@@ -31,7 +37,8 @@ func _physics_process(delta: float) -> void:
 		$attack.flip_v = false
 		$Sprite.flip_h = true
 		
-	if Input.is_action_just_pressed("atk") and can_do == true:
+	if Input.is_action_just_pressed("atk") and can_do == true and can_atk == true:
+		$"atk cooldown".start()
 		$attack.play("default")
 		
 	if $early.has_overlapping_areas() == true:
@@ -44,9 +51,10 @@ func _physics_process(delta: float) -> void:
 		hit_val = 0
 		
 		
-	if Input.is_action_just_pressed("atk") and hit_val > 0 and not is_on_floor() and can_do == true:
+	if Input.is_action_just_pressed("atk") and hit_val > 0 and not is_on_floor() and can_do == true and can_atk == true:
 		kill()
 		velocity.y = 0
+		$"atk cooldown".stop()
 		if hit_val == 2:
 			velocity.y += JUMP_VELOCITY * 1.2
 		else:
@@ -74,5 +82,7 @@ func kill():
 	$jelly/col_kill.disabled = false
 	await get_tree().create_timer(0.05).timeout
 	$jelly/col_kill.disabled = true
-	
-	
+
+
+func _on_atk_cooldown_timeout() -> void:
+	can_atk = true
